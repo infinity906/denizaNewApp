a39e1c51c71dff3cb43b4060b19a74d4c014187c
Pratik Shetty <pratik906@gmail.com>
2018-01-03 13:32:42 -0500

    Creating a bestseller tab alongwith sliding component

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-@Component({
+import { ProductProvider } from "../../providers/product/product";
+@ Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+public bestsellerproducts = [];
+  constructor(private productprovider: ProductProvider, public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad(){
+    this.productprovider.getProducts()
+      .subscribe((allproducts) => {
+        this.bestsellerproducts = allproducts.filter(product => product.bestSeller == true);
+        console.log(this.bestsellerproducts)
+      })
   }
 
 }
