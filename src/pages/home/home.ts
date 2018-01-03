52ccd20622019d341c52e81f74e570bff3b2bcf5
Pratik Shetty <pratik906@gmail.com>
2018-01-03 10:45:49 -0500

    importing data into our app using provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { ProductProvider } from "../../providers/product/product";
 
 @Component({
   selector: 'page-home',
@@ -7,7 +10,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private productProvider: ProductProvider, private http: Http, public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+    this.productProvider.getProducts()
+    .subscribe(response => console.log(response))
 
   }
 
